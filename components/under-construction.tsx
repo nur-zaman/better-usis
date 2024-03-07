8948560f763704cf18fcbeec6d5e956c600d1106
nuruzzaman <nuruzzaman@businessaccelerate.com.bd>
2024-03-07 17:37:38 +0600

    fix: set predicted CGPA to 4 decimal points.

--- components/under-construction.tsx
+++ components/under-construction.tsx
@@ -2,7 +2,7 @@ import { Construction } from "lucide-react";
 
 export default function UnderConstruction() {
   return (
-    <div className="flex flex-col items-center justify-center space-y-2 p-8 border border-dashed border-gray-200 rounded-lg border-gray-200 w-full max-w-sm mx-auto">
+    <div className="flex flex-col items-center justify-center space-y-2 p-8 border border-dashed border-gray-200 rounded-lg w-full max-w-sm mx-auto">
       <div className="flex items-center justify-center w-12 h-12 rounded-full bg-gray-100">
         <Construction strokeWidth={1} className="w-6 h-6 text-gray-500" />
       </div>
